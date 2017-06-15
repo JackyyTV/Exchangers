c86962ae6f05be3be5973a320bdfd639cabb4fdf
Jacky1356400 <jacky1356400@hotmail.com>
2017-06-15 18:10:16 +0800

    1.10.2 Port
    I've only ported the new textures and the Tuberous Exchangers. The
    actual exchanger code is still being worked on.

--- src/main/java/me/jacky1356400/exchangers/Recipes.java
+++ src/main/java/me/jacky1356400/exchangers/Recipes.java
@@ -16,32 +16,44 @@ public class Recipes {
     public static void init(){
         ItemStack N = new ItemStack(Items.NETHER_STAR);
         ItemStack EYE = new ItemStack(Items.ENDER_EYE);
-        Item EC = ExchangersItems.exchangerCore;
+        ItemStack PO = new ItemStack(Items.POTATO);
+        Item EC1 = ExchangersItems.exchangerCoreT1;
+        Item EC2 = ExchangersItems.exchangerCoreT2;
+        Item EC3 = ExchangersItems.exchangerCoreT3;
         String P = "enderpearl";
+        String R = "dyeRed";
+        String L = "gemLapis";
         String D = "gemDiamond";
         String E = "gemEmerald";
         String I = "ingotIron";
         String G = "ingotGold";
+        String GN = "nuggetGold";
         String O = "obsidian";
         String S = "stone";
         String W = "logWood";
 
-        list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.exchangerCore, 3, 3,
-                new Object[] {D, P, E, P, N, P, E, P, D}));
+        list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.exchangerCoreT1, 3, 3,
+                new Object[] {W, R, W, R, P, R, W, R, W}));
+        list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.exchangerCoreT2, 3, 3,
+                new Object[] {I, L, I, EC1, EYE, EC1, I, L, I}));
+        list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.exchangerCoreT3, 3, 3,
+                new Object[] {D, EC2, E, EC2, N, EC2, E, EC2, D}));
+        list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.tuberousExchanger, 3, 3,
+                new Object[] {PO, GN, PO, PO, P, PO, PO, GN, PO}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.woodenExchanger, 3, 3,
-                new Object[] {W, EYE, W, W, EC, W, W, EYE, W}));
+                new Object[] {W, EYE, W, W, EC1, W, W, EYE, W}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.stoneExchanger, 3, 3,
-                new Object[] {S, EYE, S, S, EC, S, S, EYE, S}));
+                new Object[] {S, EYE, S, S, EC1, S, S, EYE, S}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.goldenExchanger, 3, 3,
-                new Object[] {G, EYE, G, G, EC, G, G, EYE, G}));
+                new Object[] {G, EYE, G, G, EC2, G, G, EYE, G}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.ironExchanger, 3, 3,
-                new Object[] {I, EYE, I, I, EC, I, I, EYE, I}));
+                new Object[] {I, EYE, I, I, EC2, I, I, EYE, I}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.diamondExchanger, 3, 3,
-                new Object[] {D, EYE, D, D, EC, D, D, EYE, D}));
+                new Object[] {D, EYE, D, D, EC3, D, D, EYE, D}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.emeraldExchanger, 3, 3,
-                new Object[] {E, EYE, E, E, EC, E, E, EYE, E}));
+                new Object[] {E, EYE, E, E, EC3, E, E, EYE, E}));
         list.func_192709_a(RecipeHelper.addShaped(ExchangersItems.obsidianExchanger, 3, 3,
-                new Object[] {O, EYE, O, O, EC, O, O, EYE, O}));
+                new Object[] {O, EYE, O, O, EC3, O, O, EYE, O}));
 
         // Keep this last
         recipes.add(list);

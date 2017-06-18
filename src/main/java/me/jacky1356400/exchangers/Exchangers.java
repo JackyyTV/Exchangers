1f60968c79eae7241bdc9d5b836b64c0486def57
Jacky1356400 <jacky1356400@hotmail.com>
2017-06-19 06:15:45 +0800

    Fixed a crash on servers

--- src/main/java/me/jacky1356400/exchangers/Exchangers.java
+++ src/main/java/me/jacky1356400/exchangers/Exchangers.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Exchangers.MODID, version = Exchangers.VERSION, name = Exchangers.MODNAME, useMetadata = true)
 public class Exchangers {
 
-    public static final String VERSION = "1.12-1.0";
+    public static final String VERSION = "1.12-1.0.1";
     public static final String MODID = "exchangers";
     public static final String MODNAME = "Exchangers";
     public static final ExchangersTab exchangersCreativeTab = new ExchangersTab();
--- src/main/java/me/jacky1356400/exchangers/Recipes.java
+++ src/main/java/me/jacky1356400/exchangers/Recipes.java
@@ -1,6 +1,5 @@
 package me.jacky1356400.exchangers;
 
-import net.minecraft.client.gui.recipebook.RecipeList;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,7 +7,6 @@ import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.util.ResourceLocation;
 
 public class Recipes {
-    private static RecipeList list = new RecipeList();
 
     private static int id = 0;
 

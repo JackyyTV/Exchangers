5a8c214aa5ba6ac883a776fab81ca1dadbbbb8e5
Jacky1356400 <jacky1356400@hotmail.com>
2017-10-28 02:20:08 +0800

    Added missing override annotation

--- src/main/java/me/jacky1356400/exchangers/handler/ExchangerHandler.java
+++ src/main/java/me/jacky1356400/exchangers/handler/ExchangerHandler.java
@@ -123,6 +123,7 @@ public class ExchangerHandler extends Item {
         stack.getTagCompound().setInteger("mode", modeSwitch);
     }
 
+    @Override
     public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
         if (!world.isRemote) {
             if (player.isSneaking()) {
